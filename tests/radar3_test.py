ef11175ac976241e0898a6ce395f00ceb1a2eb1f
Andrew Atterton <andrew.atterton@renalregistry.nhs.uk>
2022-10-22 01:32:01 +0100

    added faker data to all tables

--- tests/radar3_test.py
+++ tests/radar3_test.py
@@ -1,78 +1,175 @@
+from copyreg import dispatch_table
 from sqlmodel import SQLModel, create_engine, Session
+from faker import Faker
 
 from radar_models.radar3 import *
 
 # TODO: Convert to SQLite in memory once done with development. Maybe do something clever
 # with a .env and have the ability to switch between testing and developing
-engine = create_engine(
-    "postgresql://postgres:password@localhost:5432/radar3", echo=True
-)
+engine = create_engine("postgresql://postgres:password@localhost:5432/radar3")
 SQLModel.metadata.create_all(engine)
-
 session = Session(engine)
 
-patient = Patient(patient_comment="what a guy", test=False, control=False)
-session.add(patient)
-session.commit()
+f = Faker()
 
-classification_code = ClassificationCode(
-    classification_system="ABC",
-    classification_code="CBA",
-    classification_label="This is a code",
+# --- No Foreign Keys --- #
+
+session.add(Biomarker(biomarker_name=f.word(), biomarker_type=f.word()))
+
+session.add(
+    Cohort(
+        cohort_code=f.text(max_nb_chars=5),
+        cohort_name=f.text(max_nb_chars=20),
+        cohort_short_name=f.word(),
+    )
 )
-session.add(classification_code)
-session.commit()
 
-cohort = Cohort(
-    cohort_code="ABC", cohort_name="This cohort", cohort_short_name="cohort"
+session.add(
+    Consent(
+        consent_code=f.text(max_nb_chars=5),
+        consent_label=f.text(max_nb_chars=20),
+        is_paediatric=f.pybool(),
+        release_date=f.date(),
+        consent_url=f.url(),
+        is_retired=f.pybool(),
+    )
 )
-session.add(cohort)
-session.commit()
 
-country = Country(country_name="ABC", country_code="CBA")
-session.add(country)
-session.commit()
+session.add(Country(country_name=f.country(), country_code=f.country_code()))
 
-hospital = Hospital(
-    hospital_code="ABC",
-    hospital_name="The Big Bad Hospital",
-    hospital_short_name="BBH",
-    is_transplant_centre=True,
+session.add(
+    ClassificationCode(
+        classification_system=f.word(),
+        classification_code=f.pyint(),
+        classification_label=f.word(ext_word_list=["ICD", "SNOMED", "EDTA"]),
+    )
 )
-session.add(hospital)
-session.commit()
 
-alport_clinical_picture = AlportClinicalPicture(
-    patient_id=1,
-    date_of_picture=datetime.now(),
-    deafness_state=1,
-    deafness_date=datetime.strptime("16-01-1998", "%d-%m-%Y"),
-    hearing_aid_date=date(2001, 9, 10),
+session.add(DataSource(data_source_name=f.word()))
+
+session.add(Drug(drug_name=f.word()))
+
+session.add(DrugGroup(drug_group=f.word(), parent_drug_group_id=1))
+
+session.add(Ethnicity(ethnicity_code=f.country_code(), ethnicity_label=f.country()))
+
+session.add(
+    Hospital(
+        hospital_code=f.text(max_nb_chars=5),
+        hospital_name=f.word(),
+        hospital_short_name=f.word(),
+        is_transplant_centre=f.pybool(),
+    )
 )
-session.add(alport_clinical_picture)
-session.commit()
 
-biomarker = Biomarker(biomarker_name="something", biomarker_type="that type")
-session.add(biomarker)
-session.commit()
+session.add(Nationality(nationality_label=f.country()))
 
-biomarker_barcode = BiomarkerBarcode(
-    patient_id=1,
-    barcode="ABC123",
-    sample_date=datetime.strptime("16-01-1998", "%d-%m-%Y"),
+session.add(
+    Patient(patient_comment=f.sentence(nb_words=5), test=f.pybool(), control=f.pybool())
 )
-session.add(biomarker_barcode)
+
+session.add(Specialty(specialty=f.word()))
+
+session.add(Diagnosis(diagnosis_name=f.word(), is_retired=f.pybool()))
+
+session.add(Relation(relationship=f.word()))
+
 session.commit()
 
-biomarker_sample = BiomarkerSample(barcode_id=1, biomarker_sample_label="this thing")
-session.add(biomarker_sample)
-session.commit()
+# # --- Foreign Keys Included --- #
 
-biomarker_result = BiomarkerResult(
-    biomarker_id=1,
-    biomarker_sample_id=1,
-    biomarker_result_value=1.23456787,
-    measure_unit="mg",
+session.add(
+    AlportClinicalPicture(
+        patient_id=1,
+        date_of_picture=f.date(),
+        deafness_index=1,
+        deafness_date=f.date(),
+        hearing_aid_date=f.date(),
+    )
 )
-session.add(biomarker_result)
+
+session.add(
+    BiomarkerBarcode(
+        patient_id=1,
+        barcode=f.ean(length=8),
+        sample_date=f.date_time(),
+    )
+)
+
+session.add(
+    BiomarkerSample(
+        barcode_id=1,
+        biomarker_sample_label=f.word(),
+    )
+)
+
+session.commit()
+
+session.add(
+    BiomarkerResult(
+        biomarker_id=1,
+        biomarker_sample_id=1,
+        biomarker_result_value=f.pyfloat(left_digits=1, right_digits=3, positive=True),
+        measure_unit=f.word(ext_word_list=["mg", "ng", "ml"]),
+    )
+)
+
+session.add(
+    Consultant(
+        first_name=f.first_name(),
+        last_name=f.last_name(),
+        email=f.email(),
+        telephone_number=f.word(),
+        gmc_number=f.pyint(),
+        specialty_id=1,
+    )
+)
+
+session.add(CountryEthnicity(ethnicity_id=1, country_id=1))
+
+session.add(CountryNationality(nationality_id=1, country_id=1))
+
+session.add(
+    CurrentMedication(
+        patient_id=1,
+        cohort_id=1,
+        drug_id=1,
+        data_source_id=1,
+        recorded_date=f.date(),
+        dose_quantity=f.pyfloat(left_digits=1, right_digits=3, positive=True),
+        dose_unit=f.word(ext_word_list=["mg", "ng", "ml"]),
+        frequency=f.word(),
+        route=f.word(),
+        drug_text=f.word(),
+        dose_text=f.word(),
+    )
+)
+
+session.add(DiagnosisCode(diagnosis_id=1, classification_code_id=1))
+
+session.add(
+    Dialysis(
+        patient_id=1,
+        hospital_id=1,
+        data_source_id=1,
+        timeline_start=f.date(),
+        timeline_end=f.date(),
+        modality=f.pyint(max_value=20),
+    )
+)
+
+session.add(
+    FamilyHistory(
+        patient_id=1,
+        cohort_id=1,
+        is_parental_consanguinity=f.pybool(),
+        is_family_history=f.pybool(),
+        other_family_history=f.sentence(nb_words=10),
+    )
+)
+
+session.add(
+    FamilyHistoryRelation(family_history_id=1, relation_id=1, relative_patient_id=1)
+)
+
 session.commit()
